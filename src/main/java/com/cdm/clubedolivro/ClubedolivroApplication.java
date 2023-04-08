78de83a28d6369047240be94855dc5e986240d54
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-04-07 22:57:05 -0300

    feat: adição de configs

--- src/main/java/com/cdm/clubedolivro/ClubedolivroApplication.java
+++ src/main/java/com/cdm/clubedolivro/ClubedolivroApplication.java
@@ -2,10 +2,19 @@ package com.cdm.clubedolivro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ComponentScan(basePackages = "com.cdm.clubedolivro")
 @SpringBootApplication
-public class ClubedolivroApplication {
-
+@EnableWebMvc
+public class ClubedolivroApplication implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClubedolivroApplication.class, args);
 	}
